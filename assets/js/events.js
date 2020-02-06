258ef063f53a27d7f1e88f95c50afe31a9fee9ca
Luis Jimenez <luisrrjjcc@gmail.com>
2020-02-06 15:37:30 -0600

    empty page

---
+++ assets/js/events.js
@@ -0,0 +1,12 @@
+var events = [
+	{
+		name: 'Evento Ejemplo',
+		description: 'Un pequeño ejemplo.',
+		speaker: '',
+		day: '30',
+		time: '09:00',
+		place: 'Jardín de Ceremonias',
+		subjects: [],
+		tba: '0'
+	}
+];

32f0358c63bb9d51637a30c8625099e9206738f8
Luis Jimenez <luisrrjjcc@gmail.com>
2020-02-07 16:31:42 -0600

    Logo, botones, aspect ratio

--- assets/js/staff.js
+++ assets/js/staff.js
@@ -1,7 +1,8 @@
 var staff = [
 	{
 		name: 'Marian Villa',
-		information: 'Asesora en UI|UX de Google Launchpad y miembro calificador de SXSW 2020.',
+		short: 'Asesora en UI/UX de Google Launchpad y miembro calificador de SXSW 2020. CEO y co-fundadora de EVERSOCIAL.',
+		information: 'Es asesora en UI|UX de Google Launchpad y miembro calificador de SXSW 2020. Es CEO y co-fundadora de EVERSOCIAL agencia digital con más de 10 años en el mercado. Posee doble titulación como Diseñadora Gráfica y Comunicadora Social - Periodista de la Universidad Pontificia Bolivariana. Es Magister en Mercadeo de la universidad EAFIT donde su trabajo de grado se desarrolló sobre BIG DATA en Business Intelligence. Siempre ha trabajado de la mano de ingenieros y desarrolladores, y le apasiona el código, especialmente Javascript. Desde finales de 2016 es co-fundadora y Directora de @Pionerasdev, la Comunidad/ONG más grande de #WomenInTech en Colombia. Trabajando así, por reducir la brecha de género en carreras tecnológicas, enfocada en el cumplimiento de los ODS del Pacto Global de la ONU, específicamente en: Educación de calidad, equidad de género, trabajo decente y crecimiento económico y  reducción de las desigualdades.',
 		image: 'MarianVilla.png',
 		email: '-',
 		company: '',
@@ -9,7 +10,8 @@ var staff = [
 	},
 	{
         name: 'Isis García',
-        information: 'Course Director en Platzi. Profesora de Asignatura en la especialidad de Periodismo.',
+        short: 'Course Directo en Platzi. Profesora en la especialidad de Periodismo en la Facultad de Ciencias Políticas y Sociales de la UNAM.',
+        information: 'Course Director en Platzi. Profesora de Asignatura en la especialidad de Periodismo de la Facultad de Ciencias Políticas y Sociales de la Universidad Nacional Autónoma de México. Ganadora de la beca de emprendimiento de Santander para el programa Trepcamp en la Universidad de California, Berkeley.',
         image: 'IsisGarcia.jpg',
         email: '-',
         company: '',
@@ -17,7 +19,8 @@ var staff = [
     },
     {
 		name: 'Katherine Chauca',
-		information: 'Ingeniera Informática, G Suite Certified Deployment Specialist y Product Owner.',
+		short: 'G Suite Certified Deployment Specialist, Women Techmaker Ambassador. Coorganizadora de las comunidades GDG, GDG Cloud y Agile.',
+		information: 'Ingeniera Informática, G Suite Certified Deployment Specialist y Product Owner. Women Techmaker Ambassador y coorganizadora de las comunidades GDG Lima, GDG Cloud Lima y Agile Perú. Apasionada por compartir y empoderar comunidades profesionales poniendo foco especialmente en que las actividades sean inclusivas y fomenten la participación de más mujeres en tecnología.',
 		image: 'KatherineChauca.jpg',
 		email: '-',
 		company: '',
@@ -25,7 +28,8 @@ var staff = [
 	},
 	{
 		name: 'Adriana Moya',
-		information: 'Ingeniera de Sistemas, especialista en Ingeniería de Software, Google Developer Expert.',
+		short : 'Esepcialista en Ingeniería de Software. Google Developer Expert en la categoría Google Cloud Platform. Cloud Engineer en Globant.',
+		information: 'Ingeniera de Sistemas, especialista en Ingeniería de Software, Google Developer Expert en la categoría Google Cloud Platform, Cloud Engineer en Globant, con experiencia de más de 5 años en desarrollo usando tecnología de Open Source, Apasionada por enseñar,  aprender y compartir.  runner, ciclomontañista y amante de los gatos. Linkedin: https://www.linkedin.com/in/adrifermo/',
 		image: 'AdrianaMoya.png',
 		email: '-',
 		company: '',
@@ -33,7 +37,8 @@ var staff = [
 	},
 	{
 		name: 'Mayra Rodríguez',
-		information: 'Mayra Rodríguez es una desarrolladora web, con más de 4 años de experiencia.',
+		short: 'Desarrolladora web, Women Techmakers Ambassador, Google Developer Expert para la categoría de Angular y Web.',
+		information: 'Desarrolladora web, con más de 4 años de experiencia. Es Women Techmaker Ambassador, Google Developer Expert para la categoría de Angular y web y madre de un hermoso bebé llamado Thomas.',
 		image: 'MayraRodriguez.png',
 		email: '-',
 		company: '',

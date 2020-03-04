f220e6e7ef414b5dc2a4bbcad1a1c4644b632813
Luis Jimenez <luisrrjjcc@gmail.com>
2020-03-04 11:36:07 -0600

    Agenda viernes

--- assets/js/events.js
+++ assets/js/events.js
@@ -1,26 +1,5 @@
 var events = [
-/*
-	{
-		name: 'Talleres técnicos',
-		description: 'Cloud computing y front-end development.',
-		speaker: '',
-		day: '13',
-		time: '10:00',
-		place: 'Universidad Galileo',
-		subjects: [],
-		tba: '0'
-	},
-*/
-	{
-		name: 'Talleres soft-skills',
-		description: 'Design thinking, #IamRemarkable y periodismo digital.',
-		speaker: '',
-		day: '13',
-		time: '16:00',
-		place: 'Universidad Galileo',
-		subjects: [],
-		tba: '0'
-	},
+
 	{
 		name: 'Charlas',
 		description: 'Charlas y keynote.',
@@ -30,5 +9,194 @@ var events = [
 		place: 'AGEXPORT',
 		subjects: [],
 		tba: '0'
-	}
+	},
+
+
+	/* Viernes 16:00 */
+	{
+		name: 'Tips para crecer profesionalmente. Rápido.',
+		description: '',
+		speaker: 'Mayra Rodríguez',
+		day: '13',
+		time: '16:00',
+		place: 'Salón 401 - UG',
+		subjects: [],
+		tba: '0'
+	},
+	{
+		name: 'TBD',
+		description: '',
+		speaker: 'Kathy Chauca',
+		day: '13',
+		time: '16:00',
+		place: 'Salón 511 - UG',
+		subjects: [],
+		tba: '0'
+	},
+
+	/* Viernes 16:25 */
+	{
+		name: 'Haz un pívot en tu carrera profesional hacia la tecnología',
+		description: '',
+		speaker: 'Isis García',
+		day: '13',
+		time: '16:25',
+		place: 'Salón 401 - UG',
+		subjects: [],
+		tba: '0'
+	},
+	{
+		name: 'El arte de no ser odiada',
+		description: '',
+		speaker: 'Gabriela Grajeda Girón',
+		day: '13',
+		time: '16:25',
+		place: 'Salón 511 - UG',
+		subjects: [],
+		tba: '0'
+	},
+
+	/* Viernes 16:50 */
+	{
+		name: 'El rol del storytelling en el marketing de contenidos como herramienta para mujeres emprendedoras',
+		description: '',
+		speaker: 'Ivette González Salazar',
+		day: '13',
+		time: '16:50',
+		place: 'Salón 401 - UG',
+		subjects: [],
+		tba: '0'
+	},
+	{
+		name: 'Mi video con calidad profesional desde el móvil y con telepromter BIGVU',
+		description: '',
+		speaker: 'Sandra Escobar',
+		day: '13',
+		time: '16:00',
+		place: 'Salón 511 - UG',
+		subjects: [],
+		tba: '0'
+	},
+
+	/* Viernes 17:15 */
+	{
+		name: 'Empoderamiento de las mujeres en la energía solar',
+		description: '',
+		speaker: 'Beverly Morales',
+		day: '13',
+		time: '17:15',
+		place: 'Salón 401 - UG',
+		subjects: [],
+		tba: '0'
+	},
+	{
+		name: 'Marketing disruptivo / Marketing model (Taller con Wake Up Brain)',
+		description: '',
+		speaker: 'Andrea Cab',
+		day: '13',
+		time: '17:15',
+		place: 'Salón 511 - UG',
+		subjects: [],
+		tba: '0'
+	},
+
+	/* Viernes 17:35 */
+	{
+		name: 'This is UX',
+		description: '',
+		speaker: 'María Olga Samperio',
+		day: '13',
+		time: '17:35',
+		place: 'Salón 401 - UG',
+		subjects: [],
+		tba: '0'
+	},
+	{
+		name: '¿Arte o diseño? No, comunicación',
+		description: '',
+		speaker: 'Nora Pérez',
+		day: '13',
+		time: '17:35',
+		place: 'Salón 511 - UG',
+		subjects: [],
+		tba: '0'
+	},
+	{
+		name: 'Transformación digital',
+		description: '',
+		speaker: 'Bessy Chan',
+		day: '13',
+		time: '17:35',
+		place: 'Salón AC07 - UG',
+		subjects: [],
+		tba: '0'
+	},
+
+	/* Viernes 17:50 */
+	{
+		name: 'Currículo en tecnología y experiencia en equipos de robótica',
+		description: '',
+		speaker: 'María Rebeca Mendizabal Rivera',
+		day: '13',
+		time: '17:50',
+		place: 'Salón 401 - UG',
+		subjects: [],
+		tba: '0'
+	},
+	{
+		name: 'Manual de supervivencia de un ilustrador',
+		description: '',
+		speaker: 'Holly Milián',
+		day: '13',
+		time: '17:50',
+		place: 'Salón 511 - UG',
+		subjects: [],
+		tba: '0'
+	},
+	{
+		name: 'Emprendiendo mi vida nueva',
+		description: '',
+		speaker: 'Grace Marie Galicia',
+		day: '13',
+		time: '17:50',
+		place: 'Salón 401 - UG',
+		subjects: [],
+		tba: '0'
+	},
+
+	/* Viernes 18:05 */
+	{
+		name: 'Equilibrio de empresaria, mujer y otros problemas',
+		description: '',
+		speaker: 'Regina del Carmen Davila Andrade',
+		day: '13',
+		time: '18:05',
+		place: 'Salón 511 - UG',
+		subjects: [],
+		tba: '0'
+	},
+
+	/* Viernes 18:25 */
+	{
+		name: 'La importancia del diseño y el contenido digital',
+		description: '',
+		speaker: 'Alma Peraza',
+		day: '13',
+		time: '18:25',
+		place: 'Salón 511 - UG',
+		subjects: [],
+		tba: '0'
+	},
+
+	/* Viernes 18:45 */
+	{
+		name: 'Panel',
+		description: '',
+		speaker: '',
+		day: '13',
+		time: '18:45',
+		place: 'Salón 511 - UG',
+		subjects: [],
+		tba: '0'
+	},
 ];

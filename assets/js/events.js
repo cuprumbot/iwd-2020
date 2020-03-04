90c0c1d28e19453388250bddb2753cf7f400203a
Luis Jimenez <luisrrjjcc@gmail.com>
2020-03-04 11:53:22 -0600

    Agenda sábado

--- assets/js/events.js
+++ assets/js/events.js
@@ -14,7 +14,7 @@ var events = [
 	{
 		name: 'TBD',
 		description: '',
-		speaker: 'Kathy Chauca',
+		speaker: 'Katherine Chauca',
 		day: '13',
 		time: '16:00',
 		place: 'Salón 511 - UG',
@@ -60,7 +60,7 @@ var events = [
 		description: '',
 		speaker: 'Sandra Escobar',
 		day: '13',
-		time: '16:00',
+		time: '16:50',
 		place: 'Salón 511 - UG',
 		subjects: [],
 		tba: '0'
@@ -190,16 +190,101 @@ var events = [
 
 
 
-
-
+	/* Sábado 8:10 */
 	{
-		name: 'Charlas',
-		description: 'Charlas y keynote.',
-		speaker: '',
+		name: 'Arma tu propio entorno de desarrollo en la nube',
+		description: '',
+		speaker: 'Adriana Moya',
 		day: '14',
-		time: '08:00',
-		place: 'AGEXPORT',
+		time: '08:10',
+		place: 'Salón 1 - AGEXPORT',
 		subjects: [],
 		tba: '0'
-	}
+	},
+
+	/* Sábado 8:50 */
+	{
+		name: 'La anatomía de la pena: Por qué no decimos lo que queremos y cómo eso nos detiene',
+		description: '',
+		speaker: 'Andrea Noemí Mazariegos Rivas',
+		day: '14',
+		time: '08:10',
+		place: 'Salón 1 - AGEXPORT',
+		subjects: [],
+		tba: '0'
+	},
+	{
+		name: 'Biopolítica y biopoder en el siglo XXI como herramienta tecnológica para contrarrestar el fenómo del psicoplástico's,
+		description: '',
+		speaker: 'Verónica Vilma María Reyes Suruy',
+		day: '14',
+		time: '08:50',
+		place: 'Salón 2 - AGEXPORT',
+		subjects: [],
+		tba: '0'
+	},
+	{
+		name: 'Tips para encontrar trabajo 100% remoto',
+		description: '',
+		speaker: 'Ivonne Aldana',
+		day: '14',
+		time: '08:50',
+		place: 'Salón 3 - AGEXPORT',
+		subjects: [],
+		tba: '0'
+	},
+
+	/* Sábado 9:10 */
+	{
+		name: 'Aprende a comuniar tus ideas aprovechando al máximo tus habilidades técnicas',
+		description: '',
+		speaker: 'Yolanda Patricia López Solis',
+		day: '14',
+		time: '09:10',
+		place: 'Salón 2 - AGEXPORT',
+		subjects: [],
+		tba: '0'
+	},
+		{
+		name: '+Humana +Digital',
+		description: '',
+		speaker: 'Massiel Calderón',
+		day: '14',
+		time: '09:10',
+		place: 'Salón 3 - AGEXPORT',
+		subjects: [],
+		tba: '0'
+	},
+
+	/* Sábado 9:30 */
+	{
+		name: 'The effective engineer',
+		description: '',
+		speaker: 'Cecilia Castillo',
+		day: '14',
+		time: '09:30',
+		place: 'Salón 1 - AGEXPORT',
+		subjects: [],
+		tba: '0'
+	},
+	{
+		name: 'Fotografía urbana desde una perspectiva femenina',
+		description: '',
+		speaker: 'María Fernanda Alvarado Castillo',
+		day: '14',
+		time: '09:30',
+		place: 'Salón 2 - AGEXPORT',
+		subjects: [],
+		tba: '0'
+	},
+		{
+		name: 'Project management',
+		description: '',
+		speaker: 'Lucía Lucero',
+		day: '14',
+		time: '09:30',
+		place: 'Salón 3 - AGEXPORT',
+		subjects: [],
+		tba: '0'
+	},
 ];
